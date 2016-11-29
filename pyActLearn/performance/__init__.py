154b9f807c1c32d1f6990358af7115a8ca6d63f0
Tinghui Wang <tinghui.wang@wsu.edu>
2016-11-28 19:20:42 -0800

    performance: Add performance matrix and LearningResult
    Signed-off-by: Tinghui Wang <tinghui.wang@wsu.edu>

---
+++ pyActLearn/performance/__init__.py
@@ -0,0 +1,171 @@
+import logging
+import numpy as np
+
+logger = logging.getLogger(__file__)
+
+per_class_performance_index = ['true_positive', 'true_negative', 'false_positive', 'false_negative',
+                               'accuracy', 'misclassification', 'recall', 'false positive rate',
+                               'specificity', 'precision', 'prevalence', 'f-1 measure', 'g-measure']
+
+overall_performance_index = ['average accuracy', 'weighed accuracy',
+                             'precision (micro)', 'recall (micro)', 'f-1 score (micro)',
+                             'precision (macro)', 'recall (macro)', 'f-1 score (macro)',
+                             'exact matching ratio']
+
+
+def get_confusion_matrix(num_classes, label, predicted):
+    """Calculate confusion matrix based on ground truth and predicted result
+
+    Args:
+        num_classes (:obj:`int`): Number of classes
+        label (:obj:`list` of :obj:`int`): ground truth labels
+        predicted (:obj:`list` of :obj:`int`): predicted labels
+
+    Returns:
+        :class:`numpy.array`: Confusion matrix (`numpy_class` by `numpy_class`)
+    """
+    matrix = np.zeros((num_classes, num_classes))
+    for i in range(len(label)):
+        matrix[label[i]][predicted[i]] += 1
+    return matrix
+
+
+def get_performance_array(confusion_matrix):
+    """Calculate performance matrix based on the given confusion matrix
+
+    Gets performance array for each class
+    0 - True_Positive: number of samples that belong to class and classified correctly
+    1 - True_Negative: number of samples that correctly classified as not belonging to class
+    2 - False_Positive: number of samples that belong to class and not classified correctMeasure:
+    3 - False_Negative: number of samples that do not belong to class but classified as class
+    4 - Accuracy: Overall, how often is the classifier correct? (TP + TN) / (TP + TN + FP + FN)
+    5 - Misclassification: Overall, how often is it wrong? (FP + FN) / (TP + TN + FP + FN)
+    6 - Recall: When it's actually yes, how often does it predict yes? TP / (TP + FN)
+    7 - False Positive Rate: When it's actually no, how often does it predict yes? FP / (FP + TN)
+    8 - Specificity: When it's actually no, how often does it predict no? TN / (FP + TN)
+    9 - Precision: When it predicts yes, how often is it correct? TP / (TP + FP)
+    10 - Prevalence: How often does the yes condition actually occur in our sample? Total(class) / Total(samples)
+    11 - F(1) Measure: 2 * (precision * recall) / (precision + recall)
+    12 - G Measure:  sqrt(precision * recall)
+
+    Gets Overall Performance for the classifier
+    0 - Average Accuracy: The average per-class effectiveness of a classifier
+    1 - Weighed Accuracy: The average effectiveness of a classifier weighed by prevalence of each class
+    2 - Precision (micro): Agreement of the class labels with those of a classifiers if calculated from sums of per-text
+                           decision
+    3 - Recall (micro): Effectiveness of a classifier to identify class labels if calculated from sums of per-text
+                        decisions
+    4 - F-Score (micro): Relationship between data's positive labels and those given by a classifier based on a sums of
+                         per-text decisions
+    5 - Precision (macro): An average per-class agreement of the data class labels with those of a classifiers
+    6 - Recall (macro): An average per-class effectiveness of a classifier to identify class labels
+    7 - F-Score (micro): Relations between data's positive labels and those given by a classifier based on a per-class
+                         average
+    8 - Exact Matching Ratio: The average per-text exact classification
+
+    Note: In Multi-class classification, Micro-Precision == Micro-Recall == Micro-FScore == Exact Matching Ratio
+    (Multi-class classification: each input is to be classified into one and only one class)
+
+    Reference Document:
+    Sokolova, M., & Lapalme, G. (2009). A systematic analysis of performance measures for classification tasks.
+    Information Processing and Management, 45, p. 427-437
+
+    Args:
+        num_classes (:obj:`int`): Number of classes
+        confusion_matrix (:class:`numpy.array`): Confusion Matrix (numpy array of num_class by num_class)
+
+    Returns:
+        :obj:`tuple` of :class:`numpy.array`: tuple of overall performance and per class performance
+    """
+    if confusion_matrix.shape[0] != confusion_matrix.shape[1]:
+        logger.error("confusion matrix with shape " + str(confusion_matrix.shape) + " is not square.")
+        return None, None
+
+    num_classes = confusion_matrix.shape[0]
+
+    per_class = np.zeros((num_classes, len(per_class_performance_index)), dtype=float)
+    overall = np.zeros((len(overall_performance_index),), dtype=float)
+
+    for i in range(num_classes):
+        true_positive = confusion_matrix[i][i]
+        true_negative = np.sum(confusion_matrix)\
+            - np.sum(confusion_matrix[i, :])\
+            - np.sum(confusion_matrix[:, i])\
+            + confusion_matrix[i][i]
+        false_positive = np.sum(confusion_matrix[:, i]) - confusion_matrix[i][i]
+        false_negative = np.sum(confusion_matrix[i, :]) - confusion_matrix[i][i]
+        # Accuracy: (TP + TN) / (TP + TN + FP + FN)
+        per_class_accuracy = (true_positive + true_negative)\
+            / (true_positive + true_negative + false_positive + false_negative)
+        # Mis-classification: (FP + FN) / (TP + TN + FP + FN)
+        per_class_misclassification = (false_positive + false_negative)\
+            / (true_positive + true_negative + false_positive + false_negative)
+        # Recall: TP / (TP + FN)
+        if true_positive + false_negative == 0:
+            per_class_recall = 0.
+        else:
+            per_class_recall = true_positive / (true_positive + false_negative)
+        # False Positive Rate: FP / (FP + TN)
+        if false_positive + true_negative == 0:
+            per_class_fpr = 0.
+        else:
+            per_class_fpr = false_positive / (false_positive + true_negative)
+        # Specificity: TN / (FP + TN)
+        if false_positive + true_negative == 0:
+            per_class_specificity = 0.
+        else:
+            per_class_specificity = true_negative / (false_positive + true_negative)
+        # Precision: TP / (TP + FP)
+        if true_positive + false_positive == 0:
+            per_class_precision = 0.
+        else:
+            per_class_precision = true_positive / (true_positive + false_positive)
+        # prevalence
+        per_class_prevalence = (true_positive + false_negative)\
+            / (true_positive + true_negative + false_positive + false_negative)
+        # F-1 Measure: 2 * (precision * recall) / (precision +
+        if per_class_precision + per_class_recall == 0:
+            per_class_fscore = 0.
+        else:
+            per_class_fscore = 2 * (per_class_precision * per_class_recall) / (per_class_precision + per_class_recall)
+        # G Measure: sqrt(precision * recall)
+        per_class_gscore = np.sqrt(per_class_precision * per_class_recall)
+        per_class[i][0] = true_positive
+        per_class[i][1] = true_negative
+        per_class[i][2] = false_positive
+        per_class[i][3] = false_negative
+        per_class[i][4] = per_class_accuracy
+        per_class[i][5] = per_class_misclassification
+        per_class[i][6] = per_class_recall
+        per_class[i][7] = per_class_fpr
+        per_class[i][8] = per_class_specificity
+        per_class[i][9] = per_class_precision
+        per_class[i][10] = per_class_prevalence
+        per_class[i][11] = per_class_fscore
+        per_class[i][12] = per_class_gscore
+
+    # Average Accuracy: Sum{i}{Accuracy{i}} / num_class
+    overall[0] = np.sum(per_class[:, per_class_performance_index.index('accuracy')]) / num_classes
+    # Weighed Accuracy: Sum{i}{Accuracy{i} * Prevalence{i}} / num_class
+    overall[1] = np.dot(per_class[:, per_class_performance_index.index('accuracy')],
+                        per_class[:, per_class_performance_index.index('prevalence')])
+    # Precision (micro): Sum{i}{TP_i} / Sum{i}{TP_i + FP_i}
+    overall[2] = np.sum(per_class[:, per_class_performance_index.index('true_positive')]) / \
+                 np.sum(per_class[:, per_class_performance_index.index('true_positive')] +
+                        per_class[:, per_class_performance_index.index('false_positive')])
+    # Recall (micro): Sum{i}{TP_i} / Sum{i}{TP_i + FN_i}
+    overall[3] = np.sum(per_class[:, per_class_performance_index.index('true_positive')]) / \
+                 np.sum(per_class[:, per_class_performance_index.index('true_positive')] +
+                        per_class[:, per_class_performance_index.index('false_negative')])
+    # F_Score (micro): 2 * Precision_micro * Recall_micro / (Precision_micro + Recall_micro)
+    overall[4] = 2 * overall[2] * overall[3] / (overall[2] + overall[3])
+    # Precision (macro): Sum{i}{Precision_i} / num_class
+    overall[5] = np.sum(per_class[:, per_class_performance_index.index('precision')]) / num_classes
+    # Recall (macro): Sum{i}{Recall_i} / num_class
+    overall[6] = np.sum(per_class[:, per_class_performance_index.index('recall')]) / num_classes
+    # F_Score (macro): 2 * Precision_macro * Recall_macro / (Precision_macro + Recall_macro)
+    overall[7] = 2 * overall[5] * overall[6] / (overall[5] + overall[6])
+    # Exact Matching Ratio:
+    overall[8] = np.trace(confusion_matrix) / np.sum(confusion_matrix)
+    return overall, per_class
+
---
+++ pyActLearn/performance/record.py
@@ -0,0 +1,160 @@
+import time
+import logging
+import xlsxwriter
+import collections
+from . import overall_performance_index, per_class_performance_index, get_performance_array
+from ..logging import logging_name
+from ..CASAS.fuel import CASASFuel
+
+logger = logging.getLogger(__file__)
+
+
+class LearningResult:
+    """LearningResult is a class that stores results of a learning run.
+    It may be a single-shot run or a time-based analysis
+    The result structure holds the parameters for the model as well as
+    the evaluation result for easy plot.
+
+    Parameters:
+        name (:obj:`str`): Name of the learning run
+        data (:obj:`str`): Name of the dataset or description of the dataset
+        mode (:obj:`str`): valid choices are `single_shot`, `by_week` or `by_day`
+
+    Attributes:
+        name (:obj:`str`): Name of the learning run
+        data (:obj:`str`): Path to the h5py dataset directory
+        mode (:obj:`str`): valid choices are `single_shot`, `by_week` or `by_day`
+        created_time (:obj:`float`): created time since Epoch in seconds
+        modified_time (:obj:`float`): record modified time since Epoch in seconds
+        overall_performance (:class:`numpy.array`): overall performance of the learning
+        per_class_performance (:class:`numpy.array`): overall per-class performance of the learning
+        confusion_matrix (:class:`numpy.array`): overall confusion matrix
+        records (:obj:`collections.OrderedDict`): Ordered dictionary storing all records
+    """
+    def __init__(self, name='', data='', mode='single_shot'):
+        cur_time = time.time()
+        self.name = name
+        self.data = data
+        self.mode = mode
+        self.created_time = cur_time
+        self.modified_time = cur_time
+        self.overall_performance = None
+        self.per_class_performance = None
+        self.confusion_matrix = None
+        self.records = collections.OrderedDict()
+
+    def get_num_records(self):
+        """Get the length of result records in current instance
+        """
+        if self.records is None:
+            return 0
+        else:
+            return len(self.records)
+
+    def get_record_keys(self):
+        """Get List of keys to all the records
+        """
+        if self.records is None:
+            return []
+        else:
+            return self.records.keys()
+
+    def add_record(self, model, key='single_shot', confusion_matrix=None):
+        """Add a learning milestone record
+
+        Args:
+            model (:obj:`object`): snap shot of learning model parameters
+            key (:obj:`str`): key string to represent current record
+            confusion_matrix (:obj:`numpy.array`): Confusion Matrix
+        """
+        if self.get_num_records() == 0:
+            self.confusion_matrix = confusion_matrix.copy()
+        else:
+            # Check confusion matrix size
+            if confusion_matrix.shape != self.confusion_matrix.shape:
+                logger.error(logging_name(self) + ': confusion matrix shape mismatch. Original shape %s. New shape %s'
+                             % (str(self.confusion_matrix.shape), str(confusion_matrix.shape)))
+            else:
+                self.confusion_matrix += confusion_matrix
+        self.overall_performance, self.per_class_performance = get_performance_array(self.confusion_matrix)
+        overall_performance, per_class_performance = get_performance_array(confusion_matrix)
+        cur_result = {
+            'model': model,
+            'confusion_matrix': confusion_matrix,
+            'per_class_performance': per_class_performance,
+            'overall_performance': overall_performance
+        }
+        self.records[key] = cur_result
+
+    def get_record_by_key(self, key):
+        """
+        Get result corresponding to specific key
+        :param key:
+        :return:
+        """
+        if key in self.records.keys():
+            return self.records[key]
+        else:
+            logger.error(logging_name(self) + ': Cannot find record %s' % key)
+            return None
+
+    def export_to_xlsx(self, filename, home_info=None):
+        """Export to XLSX
+
+        Args:
+            filename (:obj:`str`): path to the file
+            home_info (:class:`pyActLearn.CASAS.fuel.CASASFuel`): dataset information
+        """
+        if home_info is None:
+            home_info = CASASFuel(dir_name=self.data)
+        workbook = xlsxwriter.Workbook(filename)
+        records = self.get_record_keys()
+        num_performance = len(per_class_performance_index)
+        num_classes = self.confusion_matrix.shape[0]
+        # Overall Performance Summary
+        overall_sheet = workbook.add_worksheet('overall')
+        overall_sheet.merge_range(0, 0, 0, len(overall_performance_index) - 1, 'Overall Performance')
+        for c in range(len(overall_performance_index)):
+            overall_sheet.write(1, c, str(overall_performance_index[c]))
+            overall_sheet.write(2, c, self.overall_performance[c])
+        overall_sheet.merge_range(4, 0, 4, len(per_class_performance_index), 'Per-Class Performance')
+        overall_sheet.write(5, 0, 'Activities')
+        for c in range(len(per_class_performance_index)):
+            overall_sheet.write(5, c + 1, str(per_class_performance_index[c]))
+        for r in range(num_classes):
+            label = home_info.get_activity_by_index(r)
+            overall_sheet.write(r + 6, 0, label)
+            for c in range(num_performance):
+                overall_sheet.write(r + 6, c + 1, self.per_class_performance[r][c])
+        overall_sheet.merge_range(8 + num_classes, 0, 8 + num_classes, num_classes, 'Confusion Matrix')
+        for i in range(num_classes):
+            label = home_info.get_activity_by_index(i)
+            overall_sheet.write(9 + num_classes, i + 1, label)
+            overall_sheet.write(10 + num_classes + i, 0, label)
+        for r in range(num_classes):
+            for c in range(num_classes):
+                overall_sheet.write(10 + num_classes + r, c + 1, self.confusion_matrix[r][c])
+        # Weekly Performance Summary
+        weekly_sheet = workbook.add_worksheet('weekly')
+        weekly_list_title = ['dataset', '#week'] + overall_performance_index
+        for c in range(len(weekly_list_title)):
+            weekly_sheet.write(0, c, str(weekly_list_title[c]))
+        r = 1
+        for record_id in records:
+            weekly_sheet.write(r, 0, 'b1')
+            weekly_sheet.write(r, 1, record_id)
+            for c in range(len(overall_performance_index)):
+                weekly_sheet.write(r, c + 2, '%.5f' % self.get_record_by_key(record_id)['overall_performance'][c])
+            r += 1
+        dataset_list_title = ['activities'] + per_class_performance_index
+        # Per Week Per Class Summary
+        for record_id in self.get_record_keys():
+            cur_sheet = workbook.add_worksheet(record_id)
+            for c in range(0, len(dataset_list_title)):
+                cur_sheet.write(0, c, str(dataset_list_title[c]))
+            for r in range(num_classes):
+                label = home_info.get_activity_by_index(r)
+                cur_sheet.write(r+1, 0, label)
+                for c in range(num_performance):
+                    cur_sheet.write(r + 1, c + 1, self.get_record_by_key(record_id)['per_class_performance'][r][c])
+        workbook.close()
